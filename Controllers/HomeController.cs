2c190d1488ced1ad6e79f75a13f808f67573e333
Flopinguim <08joaovitor01@gmail.com>
2024-07-22 02:22:37 -0300

    Tirando o Privacy

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -18,11 +18,6 @@ namespace aspnet_store.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
